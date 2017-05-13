ab72662feec577282ce69ad4ec4b67d0e70c05c9
Henrique Fernandes <ynhic@hotmail.com>
2017-05-13 18:56:10 -0300

    Adição de Hello world com console.writeLine e leitura de string digitadas com console.readLine

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            a = 10;
+            b = 20;
+
+            String s;
+
+            Console.Write("Hello world "+(a+b));
+
+            s = Console.ReadLine();
+
+            Console.WriteLine(s);
+
+
+            Console.ReadKey();
+        }
+    }
+}
